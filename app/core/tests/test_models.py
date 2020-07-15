91701ff76a5af52c1f486b0507af8be3f6625378
gitsapuser <alan.rubin@sappi.com>
2020-07-15 16:03:35 -0400

    commit

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,7 +1,12 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
 
+def sample_user(email='test@londonappdev.com', password='testpass'):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email, password)
+    
 class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
@@ -13,12 +18,12 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(user.email, email)
-        self.assertTrue(user.check_password(password)) 
+        self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normal_normalized(self):
         """Test the email for a new user is normalized"""
         email = 'test@test.com'
-        user = get_user_model().objects.create_user(email, 'test123') 
+        user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
 
@@ -34,7 +39,25 @@ class ModelTests(TestCase):
         user = get_user_model().objects.create_superuser(
              'test@test.com',
              'test123'
-        ) 
+        )
 
         self.assertTrue(user.is_superuser)
-        self.assertTrue(user.is_staff)  
+        self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """Test the tag string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+
+        self.assertEqual(str(tag), tag.name)
+
+    def test_ingredient_str(self):
+        """Test the ingredient string representation"""
+        ingredient = models.Ingredient.objects.create(
+            user=sample_user(),
+            name='Cucumber'
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
